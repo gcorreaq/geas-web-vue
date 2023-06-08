1826db2c9b0be62d355a15b0c0de3cae4e981b7f
Gonzalo Correa-Young <1024282+gcorreaq@users.noreply.github.com>
2023-06-07 22:58:30 -0700

    Add new component for page footer

---
+++ src/components/PageFooter.vue
@@ -0,0 +1,36 @@
+<script lang="ts">
+import SvgIcon from '@jamescoyle/vue-icon';
+import { mdiGithub, mdiSourceBranch } from '@mdi/js';
+
+interface PageFooterComponentData {
+  githubIconSvgPath: string;
+  sourceBranch: string;
+}
+
+export default {
+  components: {
+    SvgIcon,
+  },
+  data: (): PageFooterComponentData => ({
+    githubIconSvgPath: mdiGithub,
+    sourceBranch: mdiSourceBranch,
+  }),
+};
+</script>
+
+<template>
+  <div class="grid">
+    <div>
+      Made by
+      <a href="https://github.com/gcorreaq"
+        >gcorreaq <svg-icon type="mdi" :path="githubIconSvgPath"></svg-icon
+      ></a>
+    </div>
+    <div>
+      <a href="https://github.com/gcorreaq/gean-web"
+        >gean-web
+        <svg-icon type="mdi" :path="sourceBranch"></svg-icon>
+      </a>
+    </div>
+  </div>
+</template>

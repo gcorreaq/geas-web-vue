2651bcc7c05b641b840f32040aa6cdc74bda9001
Gonzalo Correa-Young <1024282+gcorreaq@users.noreply.github.com>
2026-02-04 20:52:25 -0800

    Bump esbuild, @vitejs/plugin-vue and vite (#61)

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,6 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  esbuild: {
-    supported: {
-      'top-level-await': true,
-    },
-  },
   plugins: [vue()],
   resolve: {
     alias: {

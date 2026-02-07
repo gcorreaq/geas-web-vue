200d86cce5ca07dc62478ae5d7e4a5aa9cb8961a
Gonzalo Correa-Young <1024282+gcorreaq@users.noreply.github.com>
2026-02-06 23:07:26 -0800

    feat: replace appointment table with accordion UI and locale-aware times (#74)

--- src/components/AvailableAppointmentsList.vue
+++ src/components/AvailableAppointmentsList.vue
@@ -2,9 +2,9 @@
 import AvailableAppointment from './AvailableAppointment.vue';
 import type { ApiAvailableSlots } from '../apiTypes';
 
-interface ParsedAppointment {
-  appointmentDate: Date;
-  showDate: boolean;
+interface DateGroup {
+  dateKey: string;
+  appointments: Date[];
 }
 
 export default {
@@ -13,15 +13,23 @@ export default {
   },
 
   computed: {
-    parsedAppointments(): ParsedAppointment[] {
-      return this.appointments.map((slot: ApiAvailableSlots, index: number) => {
+    groupedAppointments(): DateGroup[] {
+      const groups: DateGroup[] = [];
+      let currentGroup: DateGroup | null = null;
+
+      for (const slot of this.appointments as ApiAvailableSlots[]) {
         const appointmentDate = new Date(slot.startTimestamp);
-        const showDate =
-          index === 0 ||
-          appointmentDate.toDateString() !==
-            new Date(this.appointments[index - 1].startTimestamp).toDateString();
-        return { appointmentDate, showDate };
-      });
+        const dateKey = appointmentDate.toDateString();
+
+        if (!currentGroup || currentGroup.dateKey !== dateKey) {
+          currentGroup = { dateKey, appointments: [appointmentDate] };
+          groups.push(currentGroup);
+        } else {
+          currentGroup.appointments.push(appointmentDate);
+        }
+      }
+
+      return groups;
     },
   },
 
@@ -30,20 +38,9 @@ export default {
 </script>
 
 <template>
-  <table role="grid">
-    <thead>
-      <tr>
-        <th scope="col">Date</th>
-        <th scope="col">Time</th>
-      </tr>
-    </thead>
-    <tbody>
-      <AvailableAppointment
-        v-for="{ appointmentDate, showDate } in parsedAppointments"
-        :key="appointmentDate.getTime()"
-        :appointment-date="appointmentDate"
-        :show-date="showDate"
-      />
-    </tbody>
-  </table>
+  <AvailableAppointment
+    v-for="group in groupedAppointments"
+    :key="group.dateKey"
+    :appointments="group.appointments"
+  />
 </template>
